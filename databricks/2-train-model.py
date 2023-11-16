0d10fd08a6e2547c3a4fe6120fff0d3d93b42bfa
Leonardo Moraes AB-InBev <127417214+leonardo-moraes-inbev@users.noreply.github.com>
2023-11-16 18:41:26 -0300

    feat: add mlflow tracking (#3)
    * feat: add mlflow
    
    ---------
    
    Co-authored-by: leonardo-moraes-inbev <leonardo-moraes-inbev>

--- databricks/2-train-model.py
+++ databricks/2-train-model.py
@@ -10,11 +10,14 @@
 
 # COMMAND ----------
 
+import mlflow
 import pandas as pd
 
 from databricks import feature_store
-from surprise import Dataset, Reader, SVD
+from surprise import SVD, accuracy
+from surprise import Dataset, Reader
 from surprise.model_selection import cross_validate
+from surprise.model_selection import train_test_split
 
 # COMMAND ----------
 
@@ -35,7 +38,7 @@ ratings.display()
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Train Recommendation System
+# MAGIC ## Validating and Testing
 # MAGIC
 # MAGIC Steps:
 # MAGIC - Surprise has a set of builtin datasets, but you can create a custom dataset. Loading a rating dataset can be done either from a file (e.g. a csv file), or from a `pandas.DataFrame`. Either way, you will need to define a `Reader` object for Surprise to be able to parse the dataset.
@@ -59,3 +62,45 @@ algorithm = SVD()
 
 # Run 5-fold cross-validation and print results
 results = cross_validate(algorithm, dataset, measures=["RMSE", "MAE"], cv=5, verbose=True)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Registering Experiment
+# MAGIC MLflow is an open-source platform for managing the end-to-end machine learning lifecycle. One of the key features of MLflow is the ability to register and track experiments, which allows data scientists and machine learning practitioners to keep track of their experiments and compare the performance of different models.
+# MAGIC
+# MAGIC Steps:
+# MAGIC - Create an experiment
+# MAGIC - Train SVD model
+# MAGIC - Log trained model and results on MLflow
+
+# COMMAND ----------
+
+# Create an experiment name, which must be unique and case sensitive
+experiment = mlflow.set_experiment("/Shared/book-recommendation-system")
+
+print(f"\nName: {experiment.name}")
+print(f"Experiment_id: {experiment.experiment_id}")
+print(f"Tags: {experiment.tags}")
+print(f"Lifecycle_stage: {experiment.lifecycle_stage}")
+print(f"Creation timestamp: {experiment.creation_time}")
+
+# COMMAND ----------
+
+with mlflow.start_run(experiment_id=experiment.experiment_id):
+    # train
+    algorithm = SVD()
+    trainset, testset = train_test_split(dataset, test_size=0.2, random_state=27)
+    algorithm.fit(trainset)
+    # score
+    predictions = algorithm.test(testset)
+    rmse = accuracy.rmse(predictions,verbose=False)
+    mae = accuracy.mae(predictions,verbose=False)
+
+    # logging experiment
+    mlflow.log_metric("RMSE", rmse)
+    mlflow.log_metric("MAE", mae)
+    # track model (depends on model type)
+    mlflow.sklearn.log_model(algorithm, 'model')
+
+f'RMSE: {rmse}', f'MAE: {mae}'
